f971d0787905b78322266aa14deecf6a7f002a76
ridwanndwip <ridwanndwip@gmail.com>
2022-02-01 16:46:58 +0700

    navbar and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./docs/**/*.js" , "./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bgcolor': '#f9f9f9',
+        'textcolor' : '#454545'
+      },
+    },
   },
   plugins: [],
 }
